f54100f316b1f2b4432954a685eee71ad9a462fa
defaultbr <tarcisioambrosio@gmail.com>
2021-04-12 12:11:23 -0300

    Update windows-configure-npmrc-azure-feed.ps1

--- windows-configure-npmrc-azure-feed.ps1
+++ windows-configure-npmrc-azure-feed.ps1
@@ -14,5 +14,7 @@ switch ($a) {
 Write-Output "registry=https://pkgs.dev.azure.com/$org/_packaging/$org/npm/registry/" > .npmrc
 Write-Output "always-auth=true" >> .npmrc
 
+
+npm install -g azure-devops-npm-auth
 azure-devops-npm-auth --client_id='212f1e16-b6cc-432f-b4d6-14a5963428b9' --tenant_id='b8329613-0680-4673-a03f-9a18a0b0e93b'
 
