6fdbfa26b4873726e8f5aab24a6ff081c5141ad7
Gabriel Peixoto <gabriel.a.m.peixoto@hotmail.com>
2021-04-12 12:20:18 -0300

    Update windows-configure-npmrc-azure-feed.ps1

--- windows-configure-npmrc-azure-feed.ps1
+++ windows-configure-npmrc-azure-feed.ps1
@@ -17,6 +17,6 @@ Write-Output "always-auth=true" >> .npmrc
 
 Set-ExecutionPolicy RemoteSigned -Scope CurrentUser
 
-npm install -g azure-devops-npm-auth
-azure-devops-npm-auth --client_id='212f1e16-b6cc-432f-b4d6-14a5963428b9' --tenant_id='b8329613-0680-4673-a03f-9a18a0b0e93b'
+npm install -g vsts-npm-auth --registry https://registry.npmjs.com --always-auth false
 
+vsts-npm-auth -config .npmrc
