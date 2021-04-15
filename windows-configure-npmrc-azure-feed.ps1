052f2825005ee2c754b2edff35a724ae29805be1
defaultbr <tarcisioambrosio@gmail.com>
2021-04-15 13:33:36 -0300

    Update windows-configure-npmrc-azure-feed.ps1

--- windows-configure-npmrc-azure-feed.ps1
+++ windows-configure-npmrc-azure-feed.ps1
@@ -16,8 +16,22 @@ Remove-Item .npmrc -Force -Recurse -ErrorAction Ignore
 Remove-Item $home/.npmrc -Force -Recurse -ErrorAction Ignore
 
 
-Write-Output "registry=https://pkgs.dev.azure.com/$org/_packaging/$org/npm/registry/" | Set-Content .npmrc
+Write-Output ""
+Write-Output "Configurando o .npmrc do projeto com o feed da org selecionada"
+Write-Output "registry=https://registry.npmjs.org" | Set-Content .npmrc
+Write-Output "@midway:registry=https://pkgs.dev.azure.com/$SELECTED_ORG/_packaging/$SELECTED_ORG/npm/registry/" | Add-Content .npmrc
+Write-Output "@riachuelo:registry=https://pkgs.dev.azure.com/$SELECTED_ORG/_packaging/$SELECTED_ORG/npm/registry/" | Add-Content .npmrc
+Write-Output "@pub-libs:registry=https://pkgs.dev.azure.com/$SELECTED_ORG/_packaging/$SELECTED_ORG/npm/registry/" | Add-Content .npmrc
 Write-Output "always-auth=true" | Add-Content .npmrc
+Write-Output ""
+Write-Output "# VEJA INFORMAÇÕES SOBRE O NPMRC AQUI: https://docs.npmjs.com/cli/v7/configuring-npm/npmrc"
+Write-Output "# Leia e entenda a utilidade do npmrc e os diferentes locais que ele pode estar"
+Write-Output "# o .npmrc do projeto ficou configurado da seguinte forma:"
+Write-Output ""
+Write-Output ""
+type result.txt .npmrc
+Write-Output ""
+Write-Output ""
 
 
 Set-ExecutionPolicy RemoteSigned -Scope CurrentUser
